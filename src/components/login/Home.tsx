ff51bf525c0579ac90e1160115dced55fc67f127
Pmedina30 <pedromedina1508@icloud.com>
2025-04-24 22:04:22 -0400

    Add files via upload

---
+++ src/components/login/Home.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/Home.css';
+import '../styles/animations.css';
+
+const Home: React.FC = () => {
+  return (
+    <div className="home-container fade-in">
+      <h1>Bienvenido al Registro de Visitantes</h1>
+      <div className="button-group">
+        <Link to="/login-residente" className="home-btn">Soy Residente</Link>
+        <Link to="/login-admin" className="home-btn">Soy Administrador</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
