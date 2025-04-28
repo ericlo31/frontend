87c70746bffb2a127ce265317e8c57d9b65a2f1c
Eric Lorenzo-Almánzar <ericlorenzo31@gmail.com>
2025-04-28 19:55:02 -0400

    Mejora de diseño de los botones de acción

--- src/components/residente/quickActions.tsx
+++ src/components/residente/quickActions.tsx
@@ -1,12 +1,23 @@
 import React from "react";
+import { FaEdit, FaFileExport, FaClipboardList } from "react-icons/fa";
 import styles from "./residente.module.css";
 
 const QuickActions = () => {
   return (
-    <div className={styles.actionsWrapper}>
-      <button className={styles.actionBtn}>+ New Authorization</button>
-      <button className={styles.actionBtn}>Schedule Visit</button>
-      <button className={styles.actionBtn}>View Passes</button>
+    <div className={styles.section}>
+      <h3>Acciones Rápidas</h3>
+      <div className={styles.actionsWrapper}>
+        <button className={styles.actionBtn}>
+          <FaEdit className={styles.actionIcon} /> Crear Autorización
+        </button>
+        <button className={styles.actionBtn}>
+          <FaClipboardList className={styles.actionIcon} /> Ver Historial de
+          Visitas
+        </button>
+        <button className={styles.actionBtn}>
+          <FaFileExport className={styles.actionIcon} /> Exportar Reportes
+        </button>
+      </div>
     </div>
   );
 };
