8bd821b2fdf8165b2c2688bd6c48ec3fcb4f72f2
zeik <alexgermosen@gmail.com>
2025-05-05 18:48:54 -0400

    se añadieron y/o mejoraron mas consultas a la api

--- src/services/visitServiceApi.ts
+++ src/services/visitServiceApi.ts
@@ -1,6 +1,6 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = "http://localhost:5000/api";
 
 /* 
 Esta interface es la data que se le debe suministrar al backend para
@@ -24,32 +24,141 @@ interface VisitExitData {
   guardia: string;
 }
 
+interface reportQuery {
+  Fdesde: string;
+  Fhasta: string;
+  Nresidente: string;
+  Pvehiculo: string;
+  Vestado: string;
+  guardiaId: string;
+}
+
+// Registrar entrada de visita
 export const registerVisitEntry = async (data: VisitEntryData) => {
   try {
-    const response = await axios.post(`${API_BASE_URL}/visit/entrada`, data);
+    const response = await axios.post(`${API_BASE_URL}/visits/entry`, data);
     return response.data;
   } catch (error) {
-    console.error('Error registrando entrada:', error);
+    console.error("Error registrando entrada:", error);
     throw error;
   }
 };
 
+// Registar salida de visita
 export const registerVisitExit = async (qrId: string, data: VisitExitData) => {
   try {
-    const response = await axios.put(`${API_BASE_URL}/visit/salida/${qrId}`, data);
+    const response = await axios.put(
+      `${API_BASE_URL}/visits/exit/${qrId}`,
+      data
+    );
     return response.data;
   } catch (error) {
-    console.error('Error registrando salida:', error);
+    console.error("Error registrando salida:", error);
     throw error;
   }
 };
 
-export const getVisitByQrId = async (qrId: string) => {
+export const updateVisit = async (id: string, data: Partial<VisitEntryData>) => {
   try {
-    const response = await axios.get(`${API_BASE_URL}/visit/${qrId}`);
+    const response = await axios.put(`${API_BASE_URL}/visits/${id}`, data);
     return response.data;
   } catch (error) {
-    console.error('Error obteniendo visita:', error);
+    console.error("Error al actualizar la visita:", error);
+    throw error;
+  }
+};
+
+export const updateVisitStatus = async (id: string, status: string) => {
+  try {
+    const response = await axios.patch(`${API_BASE_URL}/visits/${id}/status/?estado=${status}`);
+    return response.data;
+  } catch (error) {
+    console.error("Error al actualizar el estado de la visita:", error);
+    throw error;
+  }
+};
+
+export const deleteVisit = async (id: string) => {
+  try {
+    const response = await axios.delete(`${API_BASE_URL}/visits/${id}`);
+    return response.data;
+  } catch (error) {
+    console.error("Error al eliminar la visita:", error);
+    throw error;
+  }
+};
+
+
+// Hace una consulta de todas las visitas registradas
+export const getAllVisits = async () => {
+  try {
+    const response = await axios.get(`${API_BASE_URL}/visits/`);
+    return response.data;
+  } catch (error) {
+    console.error("Error obteniendo visitas:", error);
+    throw error;
+  }
+};
+
+
+// Consulta una visita por su id
+export const getVisitById = async (id: string) => {
+  try {
+    const response = await axios.get(`${API_BASE_URL}/visits/${id}`);
+    return response.data;
+  } catch (error) {
+    console.error("Error obteniendo visita:", error);
+    throw error;
+  }
+};
+
+// Consulta una visita por su qrId
+export const getVisitByQrId = async (qrId: string) => {
+  try {
+    const response = await axios.get(`${API_BASE_URL}/visits/qr/${qrId}`);
+    return response.data;
+  } catch (error) {
+    console.error("Error obteniendo visita:", error);
+    throw error;
+  }
+};
+
+// Consulta todas las visitas asignadas a un id de residente
+export const getVisitsByResidentId = async (id: string) => {
+  try {
+    const response = await axios.get(`${API_BASE_URL}/visits/resident/${id}`);
+    return response.data;
+  } catch (error) {
+    console.error("Error obteniendo visitas:", error);
+    throw error;
+  }
+};
+
+// Consulta todas las visitas registradas por id de guardia
+export const getVisitsByGuardId = async (id: string) => {
+  try {
+    const response = await axios.get(`${API_BASE_URL}/visits/guard/${id}`);
+    return response.data;
+  } catch (error) {
+    console.error("Error obteniendo visitas:", error);
+    throw error;
+  }
+};
+
+
+// Genera un reporte de visitas (admin)
+export const generateVisitReport = async (query: reportQuery) => {
+  try {
+    const response = await axios.get(`${API_BASE_URL}/visits/report?
+      Fdesde=${query.Fdesde}&
+      Fhasta=${query.Fhasta}&
+      Nresidente=${query.Nresidente}&
+      Pvehiculo=${query.Pvehiculo}&
+      Vestado=${query.Vestado}&
+      guardiaId=${query.guardiaId}`);
+    return response.data;
+  } catch (error) {
+    console.error("Error generando el reporte de visitas:", error);
     throw error;
   }
 };
