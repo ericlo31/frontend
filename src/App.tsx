73fca30f7a68800332ecf7c6f40ead9d3957b7c8
Eric Lorenzo-Almánzar <ericlorenzo31@gmail.com>
2025-05-12 17:49:20 -0400

    Arreglo de css para el login

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,13 @@ import Authorizations from "./pages/Authorizations";
 import Settings from "./pages/Settings";
 import History from "./pages/History";
 import VisitForm from "./components/authorization/VisitForm";
-import Home from "./components/login/Home";
+import Home from "./components/login/ResidentLogin";
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Dashboard />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/home" element={<Dashboard />} />
       <Route path="/authorizations" element={<Authorizations />} />
       <Route path="/settings" element={<Settings />} />
       <Route path="/visit-history" element={<History />} />
