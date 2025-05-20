13f26a104be0c18e26ad2650416ea248b4838db5
zeik <alexgermosen@gmail.com>
2025-05-19 20:02:39 -0400

    implementacion de componentes y generacion de reporte

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import History from "./pages/main/History";
 import { SidebarProvider } from "./contexts/SidebarContext";
 import Home from "./pages/login/Home";
 import AdminAuthorizations from "./pages/main/AuthorizationRecords";
+import Report from "./pages/main/Report"
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
         <Route path="/settings" element={<Settings />} />
         <Route path="/visit-history" element={<History />} />
         <Route path="/admin/authorizations" element={<AdminAuthorizations />} />
+        <Route path="/admin/report" element={<Report />} />
       </Routes>
     </SidebarProvider>
   );
