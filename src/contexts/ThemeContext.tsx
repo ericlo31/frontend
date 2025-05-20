d6e5ae21d31048585084f508175cb3629d6eac41
Eric Lorenzo-Almánzar <ericlorenzo31@gmail.com>
2025-05-20 19:35:43 -0400

    Creación ThemeContext y borrar archivo innecesario

---
+++ src/contexts/ThemeContext.tsx
@@ -0,0 +1,35 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+interface ThemeContextProps {
+  theme: "light" | "dark";
+  toggleTheme: () => void;
+}
+
+const ThemeContext = createContext<ThemeContextProps>({
+  theme: "light",
+  toggleTheme: () => {},
+});
+
+export const useTheme = () => useContext(ThemeContext);
+
+export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
+  const [theme, setTheme] = useState<"light" | "dark">(() => {
+    return (localStorage.getItem("theme") as "light" | "dark") || "light";
+  });
+
+  useEffect(() => {
+    document.body.classList.remove("light", "dark");
+    document.body.classList.add(theme);
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme((prev) => (prev === "light" ? "dark" : "light"));
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
